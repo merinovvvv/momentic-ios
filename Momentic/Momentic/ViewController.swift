12029ce9dd0ba584c2cef1ebdd17bd1f14538c04
Yaraslau Merynau <87500057+merinovvvv@users.noreply.github.com>
2025-09-25 15:42:39 +0300

    xcode project is added

---
+++ Momentic/Momentic/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Momentic
+//
+//  Created by Yaraslau Merynau on 25.09.25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
